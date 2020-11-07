716c12a162dc5805c862781c782ec6d62a58f19b
pluc <pluc@ucmerced.edu>
2020-11-07 11:39:09 -0800

    first table

---
+++ bin/data.sql
@@ -0,0 +1,16 @@
+CREATE table Pokemon (
+    pokename VARCHAR(25) PRIMARY KEY,
+    isCaptured INTEGER,
+    weight INTEGER,
+    height INTEGER,
+    species VARCHAR(25),
+    gen INTEGER,
+    gender VARCHAR(25),
+    type1 VARCHAR(25),
+    type2 VARCHAR(25),
+    locationIndex INTEGER
+)
+
+
+
+
